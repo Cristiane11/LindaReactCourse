20e633f6670de8b0be1f2a77b44558b75fef30f9
Cristiane11 <bjwmcd@yahoo.com>
2020-04-08 19:30:09 -0400

    update

--- linda-react-course/src/App.js
+++ linda-react-course/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import SkiDayCounter from './SkiDayCounter';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         
         <p>
-          <SkiDayCounter/>
+          <SkiDayCounter
+          total={this.skiData.total}
+          powder={this.skiData.powder}
+          backcountry={this.skiData.backcountry}
+          goal={this.skiData.goal}
+          />
         </p>
        
       </header>
